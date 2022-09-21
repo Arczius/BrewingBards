4735b7ce07f8056bf576ced63ab73e934ea54a41
Rowin Bodegom <99056991@mydavinci.nl>
2022-09-21 13:17:46 +0200

    student spliter made and working

--- htdocs/app/Controllers/StudentCreateController.php
+++ htdocs/app/Controllers/StudentCreateController.php
@@ -8,4 +8,32 @@ class StudentCreateController extends Controller
     {
         return view('homepages/moderator/studentcreate');
     }
+    public function CreateUsers()
+    {
+        $text = $this->request->getVar('text');
+        $class = $this->request->getVar('class');
+
+        $holdArray = explode("\n", $text);
+        $splitArray = [];
+        $newSplitArray = [];
+        foreach($holdArray as $i => $data){
+            $explode = explode(";", $data);
+            array_push($splitArray, $explode);
+        }
+        foreach($splitArray as $i => $data){
+            $explode = explode(",", $data[0]);
+            $numb = count($explode)-1;
+            $tempArray= [];
+            $newTempArray =[];
+            for($i=$numb; $i >= 0; $i--){
+                array_push($tempArray,$explode[$i]);
+            }
+            $tempName = implode(' ',$tempArray);
+            array_push($newTempArray, $tempName, $data[1]);
+            array_push($newSplitArray, $newTempArray);
+        }
+        var_dump($newSplitArray);
+        die();
+        return view('Home');
+    }
 }
---
+++ htdocs/app/Views/homepages/moderator/StudentCreate.php
@@ -0,0 +1,12 @@
+<form action="<?php echo base_url(); ?>/StudentCreateController/CreateUsers" method="post">
+    <div>
+        <textarea name="text" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div>
+        <input type="class" name="class" placeholder="class" class="form-control" >
+    </div>
+    
+    <div>
+            <button type="submit" class="btn btn-success">Signin</button>
+    </div>     
+</form>
