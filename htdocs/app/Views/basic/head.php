042d28016a3478fb47a646e337daedf34369d3e6
Sven <99063753@mydavinci.nl>
2022-09-23 11:45:09 +0200

    Styling moderator home page created
    - styling linked with head
    - table mixin made
    - added styling for moderator home page

--- htdocs/app/Views/basic/head.php
+++ htdocs/app/Views/basic/head.php
@@ -5,5 +5,5 @@
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="/public/style/style.css">
 </head>
