67cdc321291b72fee0ab368fbaf6cde9a52513f2
Sven <99063753@mydavinci.nl>
2022-09-23 12:46:36 +0200

    Moderator Add class page Styling done
    - added head to create class page
    - added header to create class page
    - created a form mixin where from now on form styling can be added
    - created a succes  button

--- htdocs/app/Controllers/ClassCreateController.php
+++ htdocs/app/Controllers/ClassCreateController.php
@@ -4,8 +4,29 @@ use CodeIgniter\Controller;
   
 class ClassCreateController extends Controller
 {
+    public function __construct(){
+        helper("rememberUser");
+    }
+
     public function index()
     {
+
+        $data = [
+            'title' => "Klas aanmaken",
+            'user' => rememberUser(),
+        ];
+
+        $base_view_dir = "homepages/moderator";
+
+        echo view("basic/head", $data);
+        // unsetting the title variable so it cant be accessed after this point
+        $data['title'];
+
+
+        echo view("$base_view_dir/header", $data);
+        // unsetting the user variable so it cant be accessed after this point
+        $data['user'];
+
         return view('homepages/moderator/ClassCreate');
     }
     public function CreateClass()
