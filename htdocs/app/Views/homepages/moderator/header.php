b726d71a91f8d1a4f1227e6f5ebccc05da3364c5
Sven <99063753@mydavinci.nl>
2022-09-28 13:18:10 +0200

    logout button added to moderator header
    - added logout button to mod header
    - added some basic styling in basic.scss

--- htdocs/app/Views/homepages/moderator/header.php
+++ htdocs/app/Views/homepages/moderator/header.php
@@ -1,4 +1,4 @@
 <header class="block--dark">
-    <h3>Welkom <?php echo $user['Name'];?></h3>
-    <p>Ingelogd als Moderator</p>
+        <h3 >Welkom <?php echo $user['Name']; ?></h3>
+        <p class="half-block">Ingelogd als Moderator</p><a class="float-right" href="<?php echo base_url(); ?>/logout">Uitloggen</a>
 </header>
