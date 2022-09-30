a2163bc9c7ab65b839c89024f1b2bfad9bfc1b13
Sven <99063753@mydavinci.nl>
2022-09-30 09:57:15 +0200

    moderator home class list styling student count
    - moderator table styling updated to work with student count
    - grow items added to table mixin
    - tooltip added to basic styling

--- htdocs/app/Views/homepages/moderator/content.php
+++ htdocs/app/Views/homepages/moderator/content.php
@@ -10,10 +10,11 @@
             <span class="table__item__col mod-tbl-classname">
                 Klasnaam
             </span>
-            <span class="table__item__col mod-tbl-classname">
-                studentenaantal
+            <span class="table__item__col mod-tbl-amount link-item tooltip">
+            &#9432; Aantal
+                <span class="tooltiptext">Studenten aantal</span>
             </span>
-            <span class="table__item__col"></span>
+            <span class="table__item__col grow-item"></span>
         </div>
 
 
@@ -25,10 +26,10 @@
                 <a href="./classes/<?php echo $class['ID'] ?>" class="table__item__col link-item mod-tbl-classname">
                     &#9432; <?php echo $class['Name']; ?>
                 </a>
-                <span class="table__item__col">
+                <span class="table__item__col mod-tbl-amount">
                     <?php echo classLengthReader($class['ID']); ?>
                 </span>
-                <a href="./StudentCreate/<?php echo $class['ID'] ?>" class="table__item__col link-item">&#x2b; Studenten toevoegen</a>
+                <a href="./StudentCreate/<?php echo $class['ID'] ?>" class="table__item__col link-item grow-item">&#x2b; Studenten toevoegen</a>
             </div>
 
         <?php
