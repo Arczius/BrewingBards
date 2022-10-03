d7af7d602c68a5143371099861698eb802e2074a
Sven <99063753@mydavinci.nl>
2022-10-03 11:14:20 +0200

    box icons and col sizes added
    box icons added to head
    col sizes 1 to 12 added to basic.scss

--- htdocs/app/Views/basic/head.php
+++ htdocs/app/Views/basic/head.php
@@ -5,5 +5,6 @@
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="<?php echo base_url() ?>/style/style.css">
 </head>
