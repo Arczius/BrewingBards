1b1b61a63bfdbc158aab28e3e0bdcf30a065bfb3
Sven <99063753@mydavinci.nl>
2022-10-03 12:39:18 +0200

    Added Favicon
    - favicon image was added and connected with the head

--- htdocs/app/Views/singin.php
+++ htdocs/app/Views/singin.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="<?php echo base_url() ?>/favicon.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="<?php echo base_url() ?>/style/style.css">
