62979fec5d9e5bf1bc462b45cef86954b1a30ee8
Sven <99063753@mydavinci.nl>
2022-10-03 21:02:14 +0200

    return button made
    made a return to last page button in the headers of admin and moderator
    
    added brewing bards logo in footer

--- htdocs/app/Views/homepages/moderator/header.php
+++ htdocs/app/Views/homepages/moderator/header.php
@@ -1,4 +1,6 @@
 <header class="block--dark">
-        <h3 >Welkom <?php echo $user['Name']; ?></h3>
+        <h3>Welkom <?php echo $user['Name']; ?></h3>
         <p class="half-block">Ingelogd als Moderator</p><a class="float-right" href="<?php echo base_url(); ?>/logout">Uitloggen</a>
+        <br> <br>
+        <a href="javascript:void(0)" onclick="history.back()"><i class='bx bx-left-arrow'></i> Ga terug</a>
 </header>
