96773fbbdb685a4cd2308a157a7c6cc20f64a7d6
Arczius <70692175+Arczius@users.noreply.github.com>
2022-10-05 12:35:49 +0200

    updates to the phpdoc documentation

--- htdocs/app/Helpers/randomPasswordGen_helper.php
+++ htdocs/app/Helpers/randomPasswordGen_helper.php
@@ -4,7 +4,7 @@
  * randomPasswordGen a function that generates a random password
  *
  * @param  int $length the length of the password
- * @return string
+ * @return string gives you back the password
  */
 function randomPasswordGen( $length = 8 ){
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#";
