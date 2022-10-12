9919988bb08e6237f14785423595591a73d46b76
Sven <99063753@mydavinci.nl>
2022-10-12 09:36:07 +0200

    moderator pages from yellow to white
    - changed some classes on mod pages to remove white background

--- htdocs/app/Views/homepages/moderator/ClassCreate.php
+++ htdocs/app/Views/homepages/moderator/ClassCreate.php
@@ -1,7 +1,7 @@
-<div class="block--accent">
+<div class="block">
     <h3>Nieuwe klas aanmaken</h3>
     <br>
-    <form action="<?php echo base_url(); ?>/ClassCreateController/CreateClass" method="post" class="form--accent form--rounded">
+    <form action="<?php echo base_url(); ?>/ClassCreateController/CreateClass" method="post" class="form form--rounded">
         <div>
             <input type="text" class="form-control" name="className" placeholder="Voer een naam in">
 
