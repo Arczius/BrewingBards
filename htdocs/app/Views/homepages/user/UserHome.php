8a6deb812b155309490ffa35b31a62216346b4be
Sven <99063753@mydavinci.nl>
2022-10-17 15:21:51 +0200

    Created disabled character card and status emblems
    - added disabled colors
    - added border mixin
    - added alert markers for char-card

--- htdocs/app/Views/homepages/user/UserHome.php
+++ htdocs/app/Views/homepages/user/UserHome.php
@@ -3,7 +3,7 @@
 
 
     <!-- card item -->
-    <div class="block--main-info border--dark col-38 char-card">
+    <div class="block--main-info  border--rounded border--dark col-38 char-card">
         <div class="col-2 float-right txt_right">
             <a href="" class="ico-h2 alt-dark tooltip">
             <i class='bx bxs-pencil'></i>
@@ -12,7 +12,12 @@
             <br>
             <a href="" class="ico-h2 alt-dark tooltip">
             <i class='bx bxs-trash'></i>
-            <span class="tooltiptext--bottom">Verwijderen</span>
+            <span class="tooltiptext">Verwijderen</span>
+            </a>
+            <br>
+            <a href="" class="ico-h2 alt-dark tooltip">
+            <i class='bx bx-download' ></i>
+            <span class="tooltiptext--bottom">Downloaden</span>
             </a>
         </div>
         <div class="col-10 char-card__header">
@@ -20,21 +25,63 @@
             <h2>Class | Race</h2>
         </div>
         <br>
-        <div class="col-12 border-top--dark char-card__icons">
+        <div class="col-10 border-top--dark char-card__icons">
             <a href="" class="ico-h1 tooltip">
                 <i class='bx bxs-castle'></i>
+                <i class='bx bxs-help-circle alert-icon--red'></i>
                 <span class="tooltiptext--bottom">Voorbeeld Leerpad</span>
             </a>
             <a href="" class="ico-h1 tooltip">
                 <i class='bx bxs-cog'></i>
+                <i class='bx bxs-error-circle alert-icon--yellow'></i>
                 <span class="tooltiptext--bottom">Tweede voorbeeld</span>
             </a>
             <a href="" class="ico-h1 tooltip">
                 <i class='bx bxs-flask' ></i>
+                <i class='bx bxs-check-circle alert-icon--green'></i>
                 <span class="tooltiptext--bottom">Derde voorbeeld</span>
             </a>
         </div>
     </div>
+
+
+ <!-- card item DISABLED-->
+ <div class="block--disabled border--rounded border--disabled col-38 char-card">
+        <div class="col-2 float-right txt_right">
+            <a href="" class="ico-h2 tooltip">
+            <i class='bx bxs-pencil'></i>
+            <span class="tooltiptext">Aanpassen</span>
+            </a>
+            <br>
+            <a href="" class="ico-h2 tooltip">
+            <i class='bx bxs-trash'></i>
+            <span class="tooltiptext">Verwijderen</span>
+            </a>
+            <br>
+            <a href="" class="ico-h2 tooltip">
+            <i class='bx bx-download' ></i>
+            <span class="tooltiptext--bottom">Downloaden</span>
+            </a>
+        </div>
+        <div class="col-10 char-card__header">
+            <h1>Inactief</h1>
+            <h2>Class | Race</h2>
+        </div>
+        <br>
+        <div class="col-10 border-top--disabled char-card__icons">
+            <span class="ico-h1">
+                <i class='bx bxs-castle'></i>   
+            </span>
+            <span class="ico-h1">
+                <i class='bx bxs-cog'></i>
+            </span>
+            <span class="ico-h1">
+                <i class='bx bxs-flask' ></i>
+            </span>
+        </div>
+    </div>
+
+
 
 
 
