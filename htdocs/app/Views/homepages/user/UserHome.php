8665298bf01c02cc6771e4515e6acc0a08642df4
Sven <99063753@mydavinci.nl>
2022-10-17 13:31:33 +0200

    User character card styling refiend
    - added color schemes
    - modified template_block
    - created bb_card file
    - added styling for cards and icons
    - moved some styling from basic to correct file

--- htdocs/app/Views/homepages/user/UserHome.php
+++ htdocs/app/Views/homepages/user/UserHome.php
@@ -1,35 +1,41 @@
 <!-- flex holder -->
-<div class="block--flex">
+<div class="block--main-info block--flex">
 
 
     <!-- card item -->
-    <div class="block border--dark col-38">
+    <div class="block--main-info border--dark col-38 char-card">
         <div class="col-2 float-right txt_right">
-            <br>
+            <a href="" class="ico-h2 alt-dark tooltip">
             <i class='bx bxs-pencil'></i>
+            <span class="tooltiptext">Aanpassen</span>
+            </a>
             <br>
+            <a href="" class="ico-h2 alt-dark tooltip">
             <i class='bx bxs-trash'></i>
+            <span class="tooltiptext--bottom">Verwijderen</span>
+            </a>
         </div>
-        <div class="col-10">
-            <h1>name</h1>
-            <h2>class | race</h2>
+        <div class="col-10 char-card__header">
+            <h1>Naam</h1>
+            <h2>Class | Race</h2>
+        </div>
+        <br>
+        <div class="col-12 border-top--dark char-card__icons">
+            <a href="" class="ico-h1 tooltip">
+                <i class='bx bxs-castle'></i>
+                <span class="tooltiptext--bottom">Voorbeeld Leerpad</span>
+            </a>
+            <a href="" class="ico-h1 tooltip">
+                <i class='bx bxs-cog'></i>
+                <span class="tooltiptext--bottom">Tweede voorbeeld</span>
+            </a>
+            <a href="" class="ico-h1 tooltip">
+                <i class='bx bxs-flask' ></i>
+                <span class="tooltiptext--bottom">Derde voorbeeld</span>
+            </a>
         </div>
     </div>
 
-
-    <!-- card item -->
-    <div class="block border--dark col-38">
-        <div class="col-2 float-right txt_right">
-            <br>
-            <i class='bx bxs-pencil'></i>
-            <br>
-            <i class='bx bxs-trash'></i>
-        </div>
-        <div class="col-10">
-            <h1>name</h1>
-            <h2>class | race</h2>
-        </div>
-    </div>
 
 
     <!-- here to evenout the flex justify, do not remove -->
