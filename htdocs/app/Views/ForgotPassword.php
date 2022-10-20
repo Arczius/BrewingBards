5077cb802b5edc8be6e75a06bd21f8fde9a7a5cf
Joran Schrievers <99062987@mydavinci.nl>
2022-10-20 15:25:19 +0200

    Added reset password mail helper function for the layout of the mail

--- htdocs/app/Views/ForgotPassword.php
+++ htdocs/app/Views/ForgotPassword.php
@@ -1,7 +1,7 @@
 <div>
 
 <form method="post" action="#">
-<label for="ForgotPasswordEmail">Voer je emailadres in zodat er een nieuw wachtwoord kan worden gestuurd</label>
-<input type="email" name="ForgotPasswordEmail"  placeholder="Voer je email in" >
+<label for="Mail">Voer je emailadres in zodat er een nieuw wachtwoord kan worden gestuurd</label>
+<input type="email" name="Mail"  placeholder="Voer je email in" >
 </form>
 </div>
