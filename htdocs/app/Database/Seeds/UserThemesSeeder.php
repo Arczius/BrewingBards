b0cd5d33160ddec8d4d8d2bde5c8b61f9dfe3339
NeneIsTheName <90730140+NeneIsTheName@users.noreply.github.com>
2022-11-14 10:24:33 +0100

    made the bug in the seeder

--- htdocs/app/Database/Seeds/UserThemesSeeder.php
+++ htdocs/app/Database/Seeds/UserThemesSeeder.php
@@ -10,32 +10,32 @@ class UserThemesSeeder extends Seeder
     {
         $data = [
             [
-                'UserID' => 1,
-                'ThemeID' => 1,
+                'ClassID' => 1,
+                'LearningPathID' => 1,
             ],
             [
-                'UserID' => 2,
-                'ThemeID' => 1,
+                'ClassID' => 2,
+                'LearningPathID' => 1,
             ],
             [
-                'UserID' => 3,
-                'ThemeID' => 1,
+                'ClassID' => 3,
+                'LearningPathID' => 1,
             ],
             [
-                'UserID' => 4,
-                'ThemeID' => 1,
+                'ClassID' => 4,
+                'LearningPathID' => 1,
             ],
             [
-                'UserID' => 5,
-                'ThemeID' => 1,
+                'ClassID' => 5,
+                'LearningPathID' => 1,
             ],
         ];
 
         foreach($data as $item){
-            $UserID = $item['UserID'];
-            $ThemeID = $item['ThemeID'];
+            $ClassID = $item['ClassID'];
+            $LearningPathID = $item['LearningPathID'];
 
-            $this->db->query("INSERT INTO UserThemes (UserID, ThemeID) VALUES ($UserID, $ThemeID)");
+            $this->db->query("INSERT INTO UserThemes (ClassID, LearningPathID) VALUES ($ClassID, $LearningPathID)");
         }
     }
 }
