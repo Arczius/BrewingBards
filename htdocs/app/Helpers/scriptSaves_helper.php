8c9b29f3b313dcd3c7c52755a0fa943beeae305f
NeneIsTheName <90730140+NeneIsTheName@users.noreply.github.com>
2022-11-18 11:37:16 +0100

    verbeterde comment

--- htdocs/app/Helpers/scriptSaves_helper.php
+++ htdocs/app/Helpers/scriptSaves_helper.php
@@ -1,9 +1,14 @@
 <?php
 
+
+/**
+ * scriptSaves - a function to sanitize a post request
+ *
+ * @param  mixed $thePost the original post request
+ * @return array returns a sanitised verions of the post request
+ */
 function scriptSaves($thePost){
     $thePost = array_map('trim', $thePost);
     $thePost = array_map('strip_tags', $thePost);
     return $thePost;
 }
-
-// $thePost word hier afgegaan om te trimmen en de tags te strippen en met de array_map function word elk item van de array af gegaan daarna word die terug gereturned.
