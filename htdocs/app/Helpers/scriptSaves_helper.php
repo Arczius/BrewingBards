3466313fbd25cd27e0cbf0933abb444b56672a6a
NeneIsTheName <90730140+NeneIsTheName@users.noreply.github.com>
2022-11-18 11:30:38 +0100

    comment onder gezet van wat het doet

--- htdocs/app/Helpers/scriptSaves_helper.php
+++ htdocs/app/Helpers/scriptSaves_helper.php
@@ -5,3 +5,5 @@ function scriptSaves($thePost){
     $thePost = array_map('strip_tags', $thePost);
     return $thePost;
 }
+
+// $thePost word hier afgegaan om te trimmen en de tags te strippen en met de array_map function word elk item van de array af gegaan daarna word die terug gereturned.
