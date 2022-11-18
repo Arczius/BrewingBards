67c254806188559cca12cc56e270c80f0283def6
Sven <99063753@mydavinci.nl>
2022-11-18 11:24:01 +0100

    linked model to mailing template page
    made model for mailing template
    added model to admin controller
    used model for view page

--- htdocs/app/Views/homepages/admin/Mailing.php
+++ htdocs/app/Views/homepages/admin/Mailing.php
@@ -9,33 +9,38 @@
 </div>
 
 <!-- Template item -->
+<?php foreach ($templates as $template){ ?>
 <div class="table__item">
     <div class="table__item__col col-4">
-        <i>Student aanmaken</i>
+        <i><?php echo $template['templateName']; ?></i>
     </div>
     <span class="table__item__col grow-item"></span>
-    <a href="JavaScript:Void(0)" class='table__item__col col-1 txt_cntr link-item modal-btn' data-modal="modal-1">
+    <a href="JavaScript:Void(0)" class='table__item__col col-1 txt_cntr link-item modal-btn' data-modal="mail-modal-<?php echo $template['MailingID']; ?>">
         Bewerken <i class='bx bxs-edit-alt'></i>
     </a>
 </div>
+<?php } ?>
+
 </div>
     
     <!-- modal card -->
-    <div class="modal" data-toggle="false" data-modal="modal-1">
+    <?php foreach ($templates as $template){ ?>
+    <div class="modal" data-toggle="false" data-modal="mail-modal-<?php echo $template['MailingID']; ?>">
         <div class="modal__body block--rounded">
             <div class="block--main block--rounded">
-                <h2 class="linebox">Template: Student aanmaken</h2>
-                <a href="JavaScript:Void(0)" class="rev float-right"><i class='bx bx-x ico-h2 modal-btn' data-modal="modal-1"></i></a>
+                <h2 class="linebox"><?php echo $template['templateName']; ?></h2>
+                <a href="JavaScript:Void(0)" class="rev float-right"><i class='bx bx-x ico-h2 modal-btn' data-modal="mail-modal-<?php echo $template['MailingID']; ?>"></i></a>
             </div>
             <div class="block">
                 <h4>Vul mailing inhoud in:</h4>
                 <br>
                 <div class="editor" id="editor">
-                    <h2>basis content</h2>
+                <?php echo $template['content']; ?>
                 </div>
                 <br>
                 <button class="btn_default" onclick="test()">Toon Content code</button>
             </div>
         </div>
     </div>
+    <?php } ?>
 </div>
