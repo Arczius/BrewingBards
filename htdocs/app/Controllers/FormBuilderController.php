e17e6f324f0704cf9b5ec2d87115bdf367feb25e
Arczius <70692175+Arczius@users.noreply.github.com>
2022-11-23 10:57:25 +0100

    changed the required check to a switchcase because boolval doenst work properly

--- htdocs/app/Controllers/FormBuilderController.php
+++ htdocs/app/Controllers/FormBuilderController.php
@@ -37,9 +37,24 @@ class FormBuilderController extends Controller
             d($this->request->getPost());
             $data = [
                 'Name' => $this->request->getPost('StudyPathName'),
-                'Required' => boolval($this->request->getPost('required')),
             ];
+
+            switch($this->request->getPost('required')){
+                case 'false':
+                    $data['Required'] = false;
+                    break;
+
+                case 'true':
+                    $data['Required'] = true;
+                    break;
+
+                default:
+                    return redirect()->to('Mod/form_builder_overview');
+            }
+
             $this->StudyPathsModel->insert($data);
+
+            return redirect()->to('Mod/form_builder_overview');
         }
         else{
             echo view("$this->base_view_dir/create/form");
