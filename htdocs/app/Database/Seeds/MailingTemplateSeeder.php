9203193ab19c864f2780361fe852a6ab6bca967f
NeneIsTheName <90730140+NeneIsTheName@users.noreply.github.com>
2022-11-30 13:51:28 +0100

    lucas heeft meegedaan aan deze opdracht

--- htdocs/app/Database/Seeds/MailingTemplateSeeder.php
+++ htdocs/app/Database/Seeds/MailingTemplateSeeder.php
@@ -11,7 +11,7 @@ class MailingTemplateSeeder extends Seeder
         $TemplateName = 'Student aanmaken';
         $TemplateContent = '<h2>Welkom {USERNAME}</h2><p><br></p><p>Je kunt nu inloggen op Social Tavern met de volgende gegevens:</p><p>E-mail:<br>{MAIL}</p><p>Wachtwoord:<br>{PASSWORD}<br><br>Dankuwel voor deelnemen.<br>Met vriendelijke groet,<br>Team Social Tavern</p><p><br></p><img src="https://i.imgur.com/hkxE6Fr.png" alt="Logo" width="150">';
         $TemplateKeywords = '{USERNAME}, {PASSWORD}, {MAIL}';
-    
+//mynivbalucy.com/furrypiss
         $this->db->query("INSERT INTO `MailingTemplates` (`MailingID`, `templateName`, `keywords`, `content`) VALUES (NULL, ' $TemplateName', '$TemplateKeywords', '$TemplateContent') ");
 
         $TemplateName = 'Wachtwoord veranderen';
