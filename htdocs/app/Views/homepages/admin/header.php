07a819f9a058ea6de30777845708fff5eea37940
Arczius <70692175+Arczius@users.noreply.github.com>
2022-12-05 14:05:08 +0100

    built the new styling for the admin header

--- htdocs/app/Views/homepages/admin/header.php
+++ htdocs/app/Views/homepages/admin/header.php
@@ -1,6 +1,7 @@
-<header class="block--main">
-    <h3>Welkom <?php echo $user['Name'] ?></h3>
-    <p class="half-block">Ingelogd als Admin</p><a class="float-right rev" href="<?php echo base_url(); ?>/logout">Uitloggen</a>
-    <br> <br>
-    <a class="rev" href="javascript:void(0)" onclick="history.back()"><i class='bx bx-left-arrow'></i> Ga terug</a>
+<header class="relative w-full bg-gray-900 p-4">
+    <h3 class="text-red-700 text-3xl font-bold">Welkom <?php echo $user['Name'] ?></h3>
+    <a class="text-red-700 text-xl absolute right-4 top-4" href="<?php echo base_url(); ?>/logout">Uitloggen</a>
+    <p class="text-red-700 text-xl pb-8">Ingelogd als Admin</p>
+
+    <a class="text-red-700 text-xl flex items-center" href="javascript:void(0)" onclick="history.back()"><i class='bx bx-left-arrow'></i> Ga terug</a>
 </header>
