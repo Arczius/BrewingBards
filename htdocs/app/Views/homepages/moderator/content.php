1377ffc0e2bdadfa7a049712c14ca7a456ff08e3
Arczius <70692175+Arczius@users.noreply.github.com>
2022-12-05 21:57:13 +0100

    rebuilt the moderator page

--- htdocs/app/Views/homepages/moderator/content.php
+++ htdocs/app/Views/homepages/moderator/content.php
@@ -1,49 +1,56 @@
 <?php
 helper('classLengthReader');
 ?>
-<div class="block">
-    <h3>Klassen overzicht</h3>
-    <br>
-    <div class="table--dark table--rounded">
 
-        <div class="table__item">
-            <span class="table__item__col col-3">
-                Klasnaam
-            </span>
-            <span class="table__item__col col-1 link-item tooltip">
-                Aantal <i class='bx bx-info-circle'></i>
-                <span class="tooltiptext">Studenten aantal</span>
-            </span>
-            <span class="table__item__col grow-item">Voeg studenten toe</span>
-            <span class="table__item__col col-2">Bewerk klas</span>
-            <span class="table__item__col col-1">Verwijderen</span>
-        </div>
+<div class="block max-w-6xl mx-auto">
 
+    <div class="block px-4 md:px-2 lg:px-0 pt-4 md:pt-8 lg:pt-10">
 
+        <table class="table-auto w-full bg-gray-200">
+            <thead class="border-b bg-gray-300">
+                <tr class="w-full">
+                    <th class="pl-4 py-4 text-left text-xl">Klasnaam:</th>
+                    <th class="text-left text-xl py-4">Aantal studenten:</th>
+                    <th class="pr-4 text-left text-xl py-4">Overige:</th>
+                </tr>
+            </thead>
 
-        <?php
-        foreach ($classes as $class) {
-        ?>
-            <div class="table__item">
-                <a href="<?php echo base_url(); ?>/Mod/classes/<?php echo $class['ID'] ?>" class="table__item__col link-item col-3">
-                    <i class='bx bxs-show'></i> <?php echo $class['Name']; ?>
-                </a>
+            <tbody>
+                <?php
+                    foreach($classes as $class){
+                        ?>
+                        <tr>
+                            <td class="py-4 pl-4">
+                                <a href="<?php echo base_url(); ?>/Mod/classes/<?php echo $class['ID'] ?>">
+                                    <i class="fa-solid fa-eye"></i> <?php echo $class['Name']; ?>
+                                </a>
+                            </td>
+                            <td class="py-4">
+                                <?php echo classLengthReader($class['ID']); ?>
+                            </td>
+                            <td>
+                                <div class="flex flex-wrap">
+                                    <a href="<?php echo base_url(); ?>/Mod/StudentCreate/<?php echo $class['ID'] ?>" class="flex-grow">
+                                        <i class="fa-solid fa-user-plus"></i>
+                                    </a>
+                                    <a href="<?php echo base_url(); ?>/Mod/ClassesEdit/<?php echo $class['ID'] ?>" class="flex-grow">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
+                                    <a href="<?php echo base_url(); ?>/Mod/DeleteClass/<?php echo $class['ID'] ?>" class="flex-grow">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
 
+                        <?php
+                    }
+                ?>
 
-                <span class="table__item__col col-1">
-                    <?php echo classLengthReader($class['ID']); ?>
-                </span>
-
-                <a href="<?php echo base_url(); ?>/Mod/StudentCreate/<?php echo $class['ID'] ?>" class="table__item__col link-item grow-item"><i class='bx bx-user-plus'></i> Studenten toevoegen</a>
-                <a href="<?php echo base_url(); ?>/Mod/ClassesEdit/<?php echo $class['ID'] ?>" class="table__item__col link-item col-2"><i class='bx bxs-pencil'></i> Klas bewerken</a>
-                <a href="<?php echo base_url(); ?>/Mod/DeleteClass/<?php echo $class['ID'] ?>" class="table__item__col link-item col-1"><i class='bx bxs-trash'></i> Verwijderen</a>
-
-
-            </div>
-
-        <?php
-        }
-        ?>
+            </tbody>
+        </table>
     </div>
+
+
     <a href="<?php echo base_url(); ?>/Mod/ClassCreate" class="btn_default">Klas toevoegen</a>
 </div>
