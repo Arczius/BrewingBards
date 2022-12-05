25ab95806be4aacea098c3dfebeffaf7166ef394
Arczius <70692175+Arczius@users.noreply.github.com>
2022-12-05 20:36:20 +0100

    imported fontawesome instead of boxicons because it has more icons

--- htdocs/app/Views/basic/head.php
+++ htdocs/app/Views/basic/head.php
@@ -4,13 +4,18 @@
     <title><?= esc($title) ?></title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- setting the favicon -->
     <link rel="shortcut icon" type="image/png" href="<?php echo base_url() ?>/favicon.png"/>
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- loading the icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/style/style.css"> -->
     <link rel="stylesheet" href="<?php echo base_url()?>/dist/tailwind.css">
     <script src="<?php echo base_url() ?>/js/switch.js"></script>
     <script src="<?php echo base_url() ?>/js/modal.js"></script>
     <script src="<?php echo base_url() ?>/js/character.js"></script>
+
     <!-- textblock style -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.25.2/ui/trumbowyg.min.css"
 		integrity="sha512-K87nr2SCEng5Nrdwkb6d6crKqDAl4tJn/BD17YCXH0hu2swuNMqSV6S8hTBZ/39h+0pDpW/tbQKq9zua8WiZTA=="
