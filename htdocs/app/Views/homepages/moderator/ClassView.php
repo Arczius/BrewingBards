a51a856c8f0d7d78e2c630a636d932ca6da51706
Arczius <70692175+Arczius@users.noreply.github.com>
2022-12-06 11:05:22 +0100

    rebuilt the student table

--- htdocs/app/Views/homepages/moderator/ClassView.php
+++ htdocs/app/Views/homepages/moderator/ClassView.php
@@ -1,72 +1,71 @@
-<div class="block">
-    <h3 class="linebox" style="margin-top: 5px;">Overzicht Klas: <?php echo $ClassName?></h3>
-    <a class="float-right btn_default" href="<?php echo base_url(); ?>/Mod/AddSingleStudent/<?php echo $classId ?>">Voeg Student toe</a>
-    <br>
+<div class="block max-w-6xl mx-auto">
 
-    <br>
-    <br>
-    <div class="table--dark table--rounded">
+    <div class="block px-4 md:px-2 lg:px-0 pt-4 md:pt-8 lg:pt-10">
+        <h2 class="pl-4 font-bold text-2xl">Overzicht Klas: <?php echo $ClassName?></h2>
 
-        <div class="table__item">
-            <span class="table__item__col col-2">
-                Studenten nummer
-            </span>
-            <span class="table__item__col col-2">
-                Naam
-            </span>
-            <span class="table__item__col col-2">
-                Mail
-            </span>
-            <span class="table__item__col col-2">
-                Last Logged In
-            </span>
-            <span class="table__item__col col-1">
-                Archiveren
-            </span>
-            <span class="table__item__col col-1">
-                klas veranderen
-            </span>
-            <span class="table__item__col col-1">
-                Aanpassen
-            </span>
-            <span class="table__item__col col-1 grow-item">
-                Verwijderen
-            </span>
-        </div>
+        <a class="float-right pr-4" href="<?php echo base_url(); ?>/Mod/AddSingleStudent/<?php echo $classId ?>">Voeg Student toe</a>
 
-        <?php
-        foreach ($Students as $Student) {
-            if(!$Student["Archive"]){
-        ?>
+        <table class="table-auto w-full">
+            <thead class="border-b ">
+                <tr class="w-full bg-gray-300">
+                    <th class="pl-4 py-4 text-left text-xl">Student nummer:</th>
+                    <th class="py-4 text-left text-xl">Naam:</th>
+                    <th class="py-4 text-left text-xl">E-mail Adres:</th>
+                    <th class="py-4 text-left text-xl">Laatst ingelogd:</th>
+                    <th class="pr-4 py-4 text-left text-xl">Overige:</th>
+                </tr>
+            </thead>
 
-            <div class="table__item">
-                <p class="table__item__col  col-2">
-                    <?php echo $Student["SchoolUserName"]; ?>
-                </p>
-                <p class="table__item__col  col-2">
-                    <?php echo $Student["Name"]; ?>
-                </p>
-                <p class="table__item__col  col-2">
-                    <?php echo $Student["Mail"]; ?>
-                </p>
-                <p class="table__item__col  col-2">
-                    <?php 
-                    if($Student["TimeLastLoggedIn"] == "0000-00-00 00:00:00"){
-                        echo "nooit ingelogged";
-                    }else{
-                       echo $Student["TimeLastLoggedIn"];  
-                    }                    
-                    ?>
-                </p>                
-                <a href="<?php echo base_url(); ?>/Mod/Archive/<?php echo $Student["ID"] ?>" class="table__item__col col-1 link-item"><i class='bx bxs-box'></i></a>
-                <a href="<?php echo base_url(); ?>/Mod/SwitchClass/<?php echo $Student["ID"] ?>" class="table__item__col col-1 link-item"><i class='bx bx-shuffle'></i></a>
-                <a href="<?php echo base_url(); ?>/Mod/StudentEdit/<?php echo $Student["ID"] ?>" class="table__item__col col-1 link-item"><i class='bx bx-edit-alt'></i></a>
-                <a href="<?php echo base_url(); ?>/Mod/StudentDelete/<?php echo $Student["ID"] ?>" class="table__item__col col-1 link-item"><i class='bx bxs-trash'></i></a>
-            </div>
+            <tbody class="bg-gray-200">
+                <?php
+                    foreach($Students as $Student){
+                        if(!$Student['Archive']){
+                            ?>
+                            <tr>
+                                <td class="pl-4 py-4">
+                                    <?php echo $Student['SchoolUserName']; ?>
+                                </td>
+                                <td class="py-4">
+                                    <?php echo $Student['Name']; ?>
+                                </td>
+                                <td class="py-4">
+                                    <a class="hover:underline" href="mailto:<?php echo $Student['Mail']; ?>">
+                                        <i class="fa-solid fa-envelope"></i> <?php echo $Student['Mail']; ?>
+                                    </a>
+                                </td>
+                                <td class="py-4">
+                                    <?php
+                                        if($Student["TimeLastLoggedIn"] == "0000-00-00 00:00:00"){
+                                            echo "nooit ingelogged";
+                                        }
+                                        else{
+                                            echo $Student["TimeLastLoggedIn"];
+                                        }
+                                    ?>
+                                </td>
+                                <td class="pr-4 py-4">
+                                    <div class="flex flex-wrap">
+                                        <a href="<?php echo base_url(); ?>/Mod/Archive/<?php echo $Student["ID"] ?>" class="flex-grow">
+                                            <i class="fa-solid fa-box-archive"></i>
+                                        </a>
+                                        <a href="<?php echo base_url(); ?>/Mod/SwitchClass/<?php echo $Student["ID"] ?>" class="flex-grow">
+                                            <i class="fa-solid fa-shuffle"></i>
+                                        </a>
+                                        <a href="<?php echo base_url(); ?>/Mod/StudentEdit/<?php echo $Student["ID"] ?>" class="flex-grow">
+                                            <i class="fa-solid fa-pen-to-square"></i>
+                                        </a>
+                                        <a href="<?php echo base_url(); ?>/Mod/StudentDelete/<?php echo $Student["ID"] ?>" class="flex-grow">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+                    }
+                ?>
 
-        <?php
-            }
-        }
-        ?>
+            </tbody>
+        </table>
     </div>
 </div>
